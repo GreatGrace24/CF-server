f752df39b3b139f60872b34b0d33fa3b3a6ab879
GreatGrace24 <gracetaiwo5@gmail.com>
2024-12-25 19:15:44 +0100

    port updated for azure

--- app.js
+++ app.js
@@ -79,7 +79,12 @@ app.post("/api/scrape", async (req, res) => {
 });
 
 // Server configuration
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // Use Azure's PORT or fallback to 5000 locally
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  // Log the URL based on the environment
+  console.log(
+    `Server running on ${
+      process.env.PORT ? `http://localhost:${PORT}` : `http://localhost:5000`
+    }`
+  );
 });
